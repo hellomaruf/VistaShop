c6d024f34c5824fefe3cef141de55366d5cecc31
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-17 00:00:32 +0600

    Added price range

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,3 +1,4 @@
+import { Slider, styled } from "@material-ui/core";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -13,6 +14,15 @@ function Products() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedBrand, setSelectedBrand] = useState("");
 
+  const CustomSlider = styled(Slider)({
+    color: "#6c72ff", // This sets the color for both the track and thumb
+    "& .MuiSlider-thumb": {
+      backgroundColor: "#6c72ff",
+    },
+    "& .MuiSlider-track": {
+      backgroundColor: "#6c72ff",
+    },
+  });
   // Fetch Data by Tanstack Query------------------------------>
   const { data: productData, refetch } = useQuery({
     queryKey: ["productData"],
@@ -35,6 +45,30 @@ function Products() {
     pages.push(i);
     refetch();
   }
+
+  // Price range functionality--------------------------------->
+  const [value, setValue] = useState([2, 200]);
+
+  // Changing State when volume increases/decreases--------------------->
+  const rangeSelector = (event, newValue) => {
+    setValue(newValue);
+    const minPrice = newValue[0];
+    const maxPrice = newValue[1];
+
+    if (minPrice) {
+      const filtered = productData?.result?.filter(
+        (item) => item?.price >= minPrice
+      );
+      setFilterData(filtered);
+    }
+
+    if (maxPrice) {
+      const filtered = productData?.result?.filter(
+        (item) => item?.price <= maxPrice
+      );
+      setFilterData(filtered);
+    }
+  };
 
   const handlePrevPage = () => {
     if (currentPage > 0) {
@@ -94,7 +128,7 @@ function Products() {
       setProducts(filterData);
     }
   }, [filterData?.result, filterData]);
-  console.log(selectedCategory);
+  console.log(products);
 
   return (
     <div className="max-w-[1380px] mx-auto">
@@ -120,100 +154,128 @@ function Products() {
               />
             </svg>
           </label>
-          <div className="max-w-xl flex items-center gap-4">
-            <select
-              onChange={(e) => setSelectedCategory(e.target.value)}
-              value={selectedCategory}
-              id="category"
-              className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
-            >
-              <option value={""} selected>
-                All Category
-              </option>
-              <option value="Tea">Tea</option>
-              <option value="Milk">Milk</option>
-              <option value="Oil">Oil</option>
-              <option value="Rice">Rice</option>
-            </select>
-            <select
-              id="brand"
-              onChange={(e) => setSelectedBrand(e.target.value)}
-              className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
-            >
-              <option value={""} selected>
-                All Blands
-              </option>
-              <option value="Fresh">Fresh</option>
-              <option value="Pran">Pran</option>
-              <option value="Chashi">Chashi</option>
-              <option value="Radhuni">Radhuni</option>
-            </select>
-          </div>
         </div>
       </div>
-      <div className="grid grid-cols-4 max-w-[1380px] mx-auto gap-4 my-10">
-        {products?.map((item, index) => (
-          <div className="" key={index}>
-            <a
-              href="#"
-              className="block rounded-lg p-4 shadow-sm shadow-indigo-100"
-            >
-              <img
-                alt=""
-                src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                className="h-56 w-full rounded-md object-cover"
-              />
+      <div className="grid grid-cols-4 max-w-[1380px] mx-auto gap-6 my-10">
+        <div className="col-span-3 grid grid-cols-3 gap-6">
+          {products?.map((item, index) => (
+            <div className="" key={index}>
+              <a
+                href="#"
+                className="block rounded-lg p-4 shadow-sm shadow-indigo-100"
+              >
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  className="h-56 w-full rounded-md object-cover"
+                />
 
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
+                <div className="mt-2">
+                  <dl>
+                    <div>
+                      <dt className="sr-only">Price</dt>
 
-                    <dd className="text-base text-gray-500">$ {item?.price}</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="font-medium">{item?.productName}</dd>
-                  </div>
-                </dl>
-
-                <div className="mt-6 flex items-center justify-between gap-8 text-xs ">
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <TbCategory className="text-xl" />
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Category</p>
-
-                      <p className="font-medium">{item?.category}</p>
+                      <dd className="text-base text-gray-500">
+                        $ {item?.price}
+                      </dd>
                     </div>
-                  </div>
 
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <TbBrandNexo className="text-xl" />
+                    <div>
+                      <dt className="sr-only">Address</dt>
 
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Brand</p>
-
-                      <p className="font-medium">{item?.brandName}</p>
+                      <dd className="font-medium">{item?.productName}</dd>
                     </div>
-                  </div>
+                  </dl>
 
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <MdDateRange className="text-xl" />
+                  <div className="mt-6 flex items-center justify-between gap-8 text-xs ">
+                    <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                      <TbCategory className="text-xl" />
 
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Date</p>
+                      <div className="mt-1.5 sm:mt-0">
+                        <p className="text-gray-500">Category</p>
 
-                      <p className="font-medium">{item?.creationDate}</p>
+                        <p className="font-medium">{item?.category}</p>
+                      </div>
+                    </div>
+
+                    <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                      <TbBrandNexo className="text-xl" />
+
+                      <div className="mt-1.5 sm:mt-0">
+                        <p className="text-gray-500">Brand</p>
+
+                        <p className="font-medium">{item?.brandName}</p>
+                      </div>
+                    </div>
+
+                    <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                      <MdDateRange className="text-xl" />
+
+                      <div className="mt-1.5 sm:mt-0">
+                        <p className="text-gray-500">Date</p>
+
+                        <p className="font-medium">{item?.creationDate}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </a>
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className="col-span-1 ">
+          <div className=" space-y-4">
+            <div
+              className=""
+              style={
+                {
+                  // margin: "auto",
+                  // display: "block",
+                  // width: "fit-content",
+                }
+              }
+            >
+              <CustomSlider
+                className="text-[#6c72ff] "
+                value={value}
+                onChange={rangeSelector}
+                min={10}
+                max={500}
+                valueLabelDisplay="auto"
+              />
+              Range of Price is between {value[0]} /- and {value[1]} /-
+            </div>
+            <div className="flex items-center gap-3">
+              <select
+                onChange={(e) => setSelectedCategory(e.target.value)}
+                value={selectedCategory}
+                id="category"
+                className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
+              >
+                <option value={""} selected>
+                  All Category
+                </option>
+                <option value="Tea">Tea</option>
+                <option value="Milk">Milk</option>
+                <option value="Oil">Oil</option>
+                <option value="Rice">Rice</option>
+              </select>
+              <select
+                id="brand"
+                onChange={(e) => setSelectedBrand(e.target.value)}
+                className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
+              >
+                <option value={""} selected>
+                  All Blands
+                </option>
+                <option value="Fresh">Fresh</option>
+                <option value="Pran">Pran</option>
+                <option value="Chashi">Chashi</option>
+                <option value="Radhuni">Radhuni</option>
+              </select>
+            </div>
           </div>
-        ))}
+        </div>
       </div>
       <div className="text-center my-7">
         <button onClick={handlePrevPage} className="btn  mr-3">
